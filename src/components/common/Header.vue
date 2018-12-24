9611bbec87bbb124346b531add71d8f0d5baf604
ekko0219 <841524858@qq.com>
2018-12-24 16:47:53 +0800

    header完成~ ~

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,7 +2,7 @@
 	<div id="header">
 		<span>+</span>
 		<input type="text" id="inp" placeholder="搜索食谱/食材，烘焙/家常菜一应俱全" />
-		<span class="btn">搜索</span>
+		<img class="btn" src="https://image.hongbeibang.com/FnuB0vCfzLhV-aolTl0hdoaTCZMJ?48X48&imageView2/1/w/48/h/48" alt="" />
 	</div>
 </template>
 
@@ -21,5 +21,35 @@ export default{
 }
 </script>
 
-<style>
+<style lang="less" scoped="">
+@import url(../../style/main.less);
+#header{
+	.w(375);
+	.h(44);
+	background:#fff;
+	span:nth-child(1){
+		display: block;
+		float: left;
+		font-size: 30px;
+		line-height: 44px;
+		color: #666;
+		padding: 0 20px 0 15px;
+	};
+	#inp{
+		display: inline-block;
+		float: left;
+		width: 250px;
+		height: 22px;
+		border-radius:5px ;
+		padding: 5px 0 5px 0;
+		margin-top:5px ;
+		background: #f5f7f9;
+	};
+	.btn{
+		display: block;
+		width: 30px;
+		height: 30px;
+		padding: 5px 10px 5px 15px;
+	}
+}
 </style>
